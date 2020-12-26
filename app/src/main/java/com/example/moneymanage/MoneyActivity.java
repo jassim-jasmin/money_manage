8c045a84733d540117371a6ac5eb7b9ca63b846b
Mohammed Jassim Jasmin <mohammedjassim.jasmin@gmail.com>
2020-12-26 16:08:19 +0530

    monthly input limited to 9 digit

--- app/src/main/java/com/example/moneymanage/MoneyActivity.java
+++ app/src/main/java/com/example/moneymanage/MoneyActivity.java
@@ -8,17 +8,21 @@ import android.text.TextWatcher;
 import android.widget.TextView;
 
 public class MoneyActivity extends AppCompatActivity {
+    TextView monthly_input;
+    TextView yearly_calculated_view;
+    TextView daily_calculated_view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money);
-        TextView monthly_input = (TextView) findViewById(R.id.monthlyIncomeInput);
-        TextView yearly_calculated_view = (TextView) findViewById(R.id.yearly_calculate_view);
-        TextView daily_calculated_view = (TextView) findViewById(R.id.daily_calculated_view);
-        setYearlyIncome(monthly_input, yearly_calculated_view, daily_calculated_view);
+        monthly_input = (TextView) findViewById(R.id.monthlyIncomeInput);
+        yearly_calculated_view = (TextView) findViewById(R.id.yearly_calculate_view);
+        daily_calculated_view = (TextView) findViewById(R.id.daily_calculated_view);
+        setYearlyIncome();
     }
 
-    protected void setYearlyIncome(TextView monthly_input, TextView yearly_calculated_view, TextView daily_calculated_view){
+    protected void setYearlyIncome(){
         monthly_input.addTextChangedListener(new TextWatcher() {
             @Override
             public void afterTextChanged(Editable s) {}
@@ -37,11 +41,13 @@ public class MoneyActivity extends AppCompatActivity {
                     int input_number = Integer.parseInt(s.toString());
                     int yearly_calculated = input_number * 12;
                     float daily_calculated = input_number/22;
+
                     yearly_calculated_view.setText(Integer.toString(yearly_calculated));
                     daily_calculated_view.setText(Float.toString(daily_calculated));
                 }
                 else {
                     yearly_calculated_view.setText("0");
+                    daily_calculated_view.setText("0.0");
                 }
 
             }
